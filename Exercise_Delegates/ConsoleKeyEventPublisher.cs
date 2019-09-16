2fa93acb4bc4abb81cfdac60c7243ecabc5327ac
Joseph Kim <s198022@students.aie.edu>
2019-09-16 16:34:56 -0500

    Add project files.

---
+++ Exercise_Delegates/ConsoleKeyEventPublisher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Delegates
+{
+    class ConsoleKeyEventPublisher
+    {
+        public delegate void TKeyPressed(char key);
+
+        // Assign an empty Lamda Function
+        private TKeyPressed KeyPressedEvent = (char key) => { Console.WriteLine("List changed event received."); };
+
+        public void Subscribe(TKeyPressed func)
+        {
+            KeyPressedEvent += func;
+        }
+
+        public void Update()
+        {
+            if (Console.KeyAvailable == true)
+            {
+                char cKeyInfo = Console.ReadKey(true).KeyChar;
+                KeyPressedEvent(cKeyInfo);
+            }
+        }
+    }
+}
---
+++ Exercise_Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Delegates
+{
+    class Program
+    {
+        static bool KeyboardUpdate = false;
+
+        static void ListenforQuit(char key)
+        {
+            if (key == ' ')
+            {
+                KeyboardUpdate = true;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ConsoleKeyEventPublisher consoleKeyEventPublisher = new ConsoleKeyEventPublisher();
+            consoleKeyEventPublisher.Subscribe(ListenforQuit);
+
+            while (KeyboardUpdate == false)
+            {
+                consoleKeyEventPublisher.Update();
+            }
+
+        }
+    }
+}
